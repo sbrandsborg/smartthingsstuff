ddc50acce59030a69abb8eaaa4e4d74d7e3061c6
Steffen Brandsborg <steffen@sbrandsborg.dk>
2020-01-02 22:27:44 +0100

    First

---
+++ devicetypes/sbrandsborg/wavin-ach-9000.src/wavin-ach-9000.groovy
@@ -0,0 +1,35 @@
+/**
+ *  WavinACH9000
+ *
+ *  Copyright 2020 Steffen Brandsborg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "WavinACH9000", namespace: "sbrandsborg", author: "Steffen Brandsborg", cstHandler: true) {
+		capability "Temperature Measurement"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'temperature' attribute
+
+}
